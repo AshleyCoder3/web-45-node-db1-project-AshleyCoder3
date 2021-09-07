fa964bfc00a4bb8b20aa3f001e1cd4a5b8344d76
Ashley Burns <Ash_burn999@yahoo.com>
2021-09-06 22:36:42 -0400

    added is NaN

--- api/accounts/accounts-middleware.js
+++ api/accounts/accounts-middleware.js
@@ -9,22 +9,22 @@ exports.checkAccountPayload = (req, res, next) => {
       message: 'name and budget are required',
       status: 400
     });
-  } if (typeof name !== 'string') {
+  } else if (typeof name !== 'string') {
     next({
       message: 'name of account must be a string',
       status: 400
     });
-  } if (name.trim().length < 3 ||
-    name.trim().length < 100) {
+  } else if (name.trim().length < 3 ||
+    name.trim().length > 100) {
     next({
       message: 'name of account must be between 3 and 100',
       status: 400
     });
-  } if (typeof budget !== 'number') {
+  } else if (typeof budget !== 'number' || isNaN(budget)) {
     next({
       message: 'budget of account must be a number'
     });
-  } if (budget < 0 ||
+  } else if (budget < 0 ||
     budget > 1000000) {
     next({
       message: 'budget of account is too large or too small',
