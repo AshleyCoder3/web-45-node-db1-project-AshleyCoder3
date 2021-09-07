54483e634370e6d2e53c7bb75392e392bb4ea839
Ashley Burns <Ash_burn999@yahoo.com>
2021-09-06 22:56:20 -0400

    delete in router and model working

--- api/accounts/accounts-router.js
+++ api/accounts/accounts-router.js
@@ -40,9 +40,10 @@ router.put('/:id',
     }
   });
 
-router.delete('/:id', checkAccountId, (req, res, next) => {
+router.delete('/:id', checkAccountId, async (req, res, next) => {
   try {
-    res.json('delete accounts by id');
+    await Account.deleteById(req.params.id);
+    res.json(req.account);
   } catch (err) {
     next(err);
   }
